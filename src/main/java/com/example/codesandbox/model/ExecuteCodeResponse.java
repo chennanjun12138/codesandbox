05064fe38bc94c6c62b2d96ceb5727d0b6f839bb
chennanjun12138 <2781417315@qq.com>
2024-02-13 23:12:32 +0800

    '初次提交'

---
+++ src/main/java/com/example/codesandbox/model/ExecuteCodeResponse.java
@@ -0,0 +1,24 @@
+package com.example.codesandbox.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExecuteCodeResponse {
+    private List<String> outputList;
+    /**
+     * 执行信息
+     */
+    private String message;
+
+    private Integer status;
+
+    private  JudgeInfo judgeInfo;
+}
